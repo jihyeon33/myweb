c7d84194b06680db861427aef2c84e4943cc21ca
jihyeon33 <twinhks1@naver.com>
2023-02-26 21:00:07 +0900

    2023.2.26

--- src/main/java/examples/HelloServlet.java
+++ src/main/java/examples/HelloServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class HelloServlet
@@ -27,7 +28,13 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF8");
+		PrintWriter out =response.getWriter();
+		out.print("안녕하세요. myweb입니다.");
+		out.print("2023.2.26  :  5> 2)Servlet 작성방법-3  수강완료.");
+		out.close();
+		
 	}
 
 	/**
