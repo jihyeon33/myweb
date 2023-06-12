fd778e574893f9c4b8f7667b6938cc0eb3f090ee
jihyeon33 <twinhks1@naver.com>
2023-06-12 18:59:13 +0900

    2023.6.12

---
+++ src/main/java/kr/or/myweb/wallet/dto/StockDto.java
@@ -0,0 +1,18 @@
+package kr.or.myweb.wallet.dto;
+
+public class StockDto {
+	private String stock;
+	private int volume;
+	public String getStock() {
+		return stock;
+	}
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+}
