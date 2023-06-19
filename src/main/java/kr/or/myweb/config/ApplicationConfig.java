649d15c9e074dc012de898702efac83b299408df
jihyeon33 <twinhks1@naver.com>
2023-06-20 03:46:35 +0900

    2023.6.20

--- src/main/java/kr/or/myweb/config/ApplicationConfig.java
+++ src/main/java/kr/or/myweb/config/ApplicationConfig.java
@@ -1,5 +1,12 @@
 package kr.or.myweb.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages= {"kr.or.myweb.dao","kr.or.myweb.service"})
+@Import({DBConfig.class})
 public class ApplicationConfig {
 
 }
--- src/main/java/kr/or/myweb/config/DBConfig.java
+++ src/main/java/kr/or/myweb/config/DBConfig.java
@@ -1,14 +1,40 @@
 package kr.or.myweb.config;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
+@Configuration
+@EnableTransactionManagement
 public class DBConfig implements TransactionManagementConfigurer {
+	private String driverClassName="com.mysql.jdbc.Driver";
+	private String url="jdbc:mysql://localhost:3306/mywebdb?useUnicode=true&characterEncoding=utf8&useSSL=false";
+	private String userName="mywebuser";
+	private String password="myweb123#";
 
 	@Override
-	public TransactionManager annotationDrivenTransactionManager() {
-		// TODO Auto-generated method stub
-		return null;
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		return transactionManger();
+	}
+	@Bean
+	public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(userName);
+		dataSource.setPassword(password);
+		return dataSource;
+	}
+	@Bean
+	public PlatformTransactionManager transactionManger() {
+		return new DataSourceTransactionManager(dataSource());
 	}
 
 }
