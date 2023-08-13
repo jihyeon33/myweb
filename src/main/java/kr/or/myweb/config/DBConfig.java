0dd6c763cce72e3f607a5450ba180e6a206ae789
jihyeon33 <twinhks1@naver.com>
2023-08-14 02:01:36 +0900

    2023.8.14

--- src/main/java/kr/or/myweb/config/DBConfig.java
+++ src/main/java/kr/or/myweb/config/DBConfig.java
@@ -14,10 +14,16 @@ import org.springframework.transaction.annotation.TransactionManagementConfigure
 @EnableTransactionManagement
 public class DBConfig implements TransactionManagementConfigurer {
     
-	private String driverClassName="com.mysql.jdbc.Driver";
-	private String url="jdbc:mysql://localhost:3306/mywebdb?useUnicode=true&characterEncoding=utf8&useSSL=false";
-	private String userName="mywebuser";
-	private String password="myweb123#";	
+	//private String driverClassName="com.mysql.jdbc.Driver";  //로컬
+	//private String url="jdbc:mysql://localhost:3306/mywebdb?useUnicode=true&characterEncoding=utf8&useSSL=false";   //로컬
+	//private String url="jdbc:mysql://awseb-e-bf2qcttc27-stack-awsebrdsdatabase-9zwub6ihotoc.c2nmwwkpyqga.ap-northeast-2.rds.amazonaws.com:3306/mywebdb?useUnicode=true&characterEncoding=utf8&useSSL=false"; //aws elastic beanstalk
+	//private String userName="mywebuser";  //로컬
+	//private String password="myweb123#";  //로컬
+	
+	private String driverClassName="org.mariadb.jdbc.Driver";  //카페24
+	private String url="jdbc:mariadb://umj7-003.cafe24.com:3306/twinhks1?useUnicode=true&characterEncoding=utf8&useSSL=false";  //카페24
+	private String userName="twinhks1";     //카페24	
+	private String password="twin532899";   //카페24
 
 	@Override
 	public PlatformTransactionManager annotationDrivenTransactionManager() {
